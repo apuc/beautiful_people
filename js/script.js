0e49f3cc0d9ca79003ae29e2bd971242187c84dc
kirill <apuc06@mail.ru>
2016-03-24 14:46:42 +0300

    Первый

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+
+
+$(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').click(function() {
+        $('body,html').animate({scrollTop: 0}, 1000);
+    });
+
+    $('.smoothScroll').click(function(event) {
+        event.preventDefault();
+        var href=$(this).attr('href');
+        var target=$(href);
+        var top=target.offset().top;
+        $('html,body').animate({
+            scrollTop: top
+        }, 1000);
+    });
+});
+
